4f520d1b4c70e74cc4935acf143d5c65dc1ce197
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-03 20:10:27 -0300

    create signIn signOut flow

--- src/App/index.js
+++ src/App/index.js
@@ -1,43 +1,44 @@
 import React, { useState, useEffect } from 'react'
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import { firebaseConfig } from '../../firebase-config.js'
+import { db } from '../../firebase'
 
 export const App = () => {
-	const [userData, setUserData] = useState({})
+	const [userData, setUserData] = useState(null)
 	useEffect(() => {
-		firebase.initializeApp(firebaseConfig)
-		// onAuthStateChanged returns the unsubscribe method
-		return firebase.auth().onAuthStateChanged(user => {
-			if (user) {
+		return db.auth().onAuthStateChanged(user => {
+			console.log(user)
+			if (user)
 				setUserData({
 					email: user.email,
-					verified: user.emailVerified,
+					verified: user.emailVerified ? 'is verified' : 'is not verified',
 					signIn: user.metadata.lastSignInTime
 				})
-			}
+			else
+				setUserData(null)
 		})
 	}, [])
-	// useEffect(() => {
-	// 	firebase.initializeApp(firebaseConfig)
-	// 	const signIn = async () => {
-	// 		try {
-	// 			const result = await firebase.auth().signInWithEmailAndPassword('vitorbarbosa19@gmail.com','qw1234')
-	// 			console.log(result)
-	// 		} catch (error) {
-	// 			console.log(error)
-	// 			if (error.message)
-	// 				console.log(JSON.parse(error.message))
-	// 		}
-	// 	}
-	// 	signIn()
-	// }, [])
+	const signIn = async () => {
+		try {
+			const result = await db.auth().signInWithEmailAndPassword('vitorbarbosa1@gmail.com', 'qw1234')
+			console.log(result)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 	return (
 		<div>
-			<p>{userData.email}</p>
-			<p>{userData.verified}</p>
-			<p>{userData.signIn}</p>
+			{userData
+				?
+					<div>
+						<p>Logged in</p>
+						<p>{userData.email}</p>
+						<p>{userData.verified}</p>
+						<p>{userData.signIn}</p>					
+					</div>
+				:
+					<p>Logged out</p>
+			}
+			<input type='submit' value='signIn' onClick={signIn} />
+			<input type='submit' value='signOut' onClick={() => db.auth().signOut()} />
 		</div>
 	)
 }
