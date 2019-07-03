a23ebcb0492ae3f993b636f9ef8394ec7298a2a6
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-03 11:19:42 -0300

    add hook to set event listener for auth changes

--- src/App/index.js
+++ src/App/index.js
@@ -1,23 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 import { firebaseConfig } from '../../firebase-config.js'
 
 export const App = () => {
+	const [userData, setUserData] = useState({})
 	useEffect(() => {
 		firebase.initializeApp(firebaseConfig)
-		const createUser = async () => {
-			try {
-				const result = await firebase.auth().createUserWithEmailAndPassword('vitorbarbosa19@gmail.com','qw1234')		
-				console.log(result)
-			} catch (error) {
-				console.log(error)
-				if (error.message)
-					console.log(JSON.parse(error.message))
+		// onAuthStateChanged returns the unsubscribe method
+		return firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				setUserData({
+					email: user.email,
+					verified: user.emailVerified,
+					signIn: user.metadata.lastSignInTime
+				})
 			}
-		}
-		createUser()
+		})
 	}, [])
-	return <div>Hi</div>
+	// useEffect(() => {
+	// 	firebase.initializeApp(firebaseConfig)
+	// 	const signIn = async () => {
+	// 		try {
+	// 			const result = await firebase.auth().signInWithEmailAndPassword('vitorbarbosa19@gmail.com','qw1234')
+	// 			console.log(result)
+	// 		} catch (error) {
+	// 			console.log(error)
+	// 			if (error.message)
+	// 				console.log(JSON.parse(error.message))
+	// 		}
+	// 	}
+	// 	signIn()
+	// }, [])
+	return (
+		<div>
+			<p>{userData.email}</p>
+			<p>{userData.verified}</p>
+			<p>{userData.signIn}</p>
+		</div>
+	)
 }
